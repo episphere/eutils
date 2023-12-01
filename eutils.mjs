ff980a66f4cb3e7c31d24876359eb1c1bed2bd23
Jonas Almeida <jalmeida@mathbiol.org>
2023-12-01 08:54:23 -0500

    Update eutils.mjs

--- eutils.mjs
+++ eutils.mjs
@@ -10,14 +10,14 @@ async function eFetch(id='NM_000546',db='nuccore',retmode='text'){
     let seq = await (await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=${db}&id=${id}&rettype=fasta&retmode=${retmode}`))[`${retmode}`]()
     let seqLines = seq.split(/\n/)
     console.log(seqLines[0])
-    return seqLines.slice(1).join('')
+    return seqLines[0]+'\n'+seqLines.slice(1).join('')
 }
 
 async function eSearch(term='TP53',db='nuccore',retmax=100){
     return await (await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=${db}&term=${term}&retmax=${retmax}&retmode=json`)).json()
 }
 
-async function eInfo(db=''){ // empty query will return list
+async function eInfo(db=''){ // database info, empty query will return list
     return await (await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/einfo.fcgi?db=${db}&retmode=json`)).json()
 }
 
@@ -29,14 +29,26 @@ async function eLink(id='2606992112',db='nuccore'){ // summary for UID
     return await (await fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?db=${db}&id=${id}&retmode=json`)).json()
 }
 
-async function getNucleotideSequence(id=1519245996,retmax=100,db='nuccore'){ // note change of parameter order between retmax and db
+async function getNucleotideSequence(id=1519245996,retmax=100,db='nucleotide'){ // note change of parameter order between retmax and db
     if(id.match(/^[0-9]+$/)){ // catching numeric ids sent as strings
         id=parseInt(id)
     }
     if(typeof(id)=='string'){ // search term, such as "TP53 Homo sapiens"
         let ids= (await E.eSearch(id,db,retmax)).esearchresult.idlist
         return ids
-    }else{  // a numeric id is provided
+    }else{  // a numeric id is provided, all is good to get the actual sequence
+        return await E.eFetch(id,db)
+    }
+}
+
+async function getProteinSequence(id=1519245996,retmax=100,db='protein'){ // note change of parameter order, db is normally 'protein' sicne this is about retrieving a protein sequence
+    if(id.match(/^[0-9]+$/)){ // catching numeric ids sent as strings
+        id=parseInt(id)
+    }
+    if(typeof(id)=='string'){ // search term, such as "TP53 Homo sapiens"
+        let ids= (await E.eSearch(id,db,retmax)).esearchresult.idlist
+        return ids
+    }else{  // a numeric id is provided, all is good to get the actual sequence
         return await E.eFetch(id,db)
     }
 }
@@ -47,7 +59,8 @@ export{
     eInfo,
     eSummary,
     eLink,
-    getNucleotideSequence
+    getNucleotideSequence,
+    getProteinSequence
 }
 
 /*
@@ -62,7 +75,17 @@ sequence viewer: https://www.ncbi.nlm.nih.gov/tools/sviewer/manual-api/
 
 # Example - fetching full TP53 sequences
 
-ids = (await E.eSearch('TP53')).esearchresult.idlist    // by default db = 'nuccore'
-seq = (await E.eFetch(ids[0])) // by default db='nucleotyde'
+ids = (await E.eSearch('TP53[gene] AND (\"Homo sapiens\"[Organism]')).esearchresult.idlist    // by default db = 'nuccore'
+seq = (await E.eFetch(ids[0])) // by default db='nuccore'
+
+getNucleotideSequence simplifies this:
+
+ids = await E.getNucleotideSequence('TP53[gene] AND (\"Homo sapiens\"[Organism]')
+seq = await E.getNucleotideSequence(ids[0]) // note change of parameter order between retmax and db
+
+and so does getProteinSequence for aminoacid sequences:
+
+ids = await E.getProteinSequence('TP53[gene] Homo sapiens',10)
+seq = await E.getProteinSequence(ids[0])
 
 */
